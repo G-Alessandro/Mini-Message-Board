f71426485c163c87b1bb631246aacf61c88e23d5
G-Alessandro <galloalessandro77@gmail.com>
2024-02-21 18:32:18 +0100

    Restarted project using pug template language

--- Mini-Message-Board/routes/index.js
+++ Mini-Message-Board/routes/index.js
@@ -1,34 +1,9 @@
-const express = require('express');
-
-const router = express.Router();
-
-const messages = [
-  {
-    text: 'Hi there!',
-    user: 'Amando',
-    added: new Date(),
-  },
-  {
-    text: 'Hello World!',
-    user: 'Charles',
-    added: new Date(),
-  },
-];
+var express = require('express');
+var router = express.Router();
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Mini Messageboard', messages });
-});
-
-router.get('/new', (req, res, next) => {
-  res.render('form');
-});
-
-router.post('/new', (req, res, next) => {
-  const authorName = req.body.author;
-  const messageText = req.body.message;
-  messages.push({ text: messageText, user: authorName, added: new Date() });
-  res.redirect('/');
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
